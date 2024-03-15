a7163b47d992e5ddf590b3269d5f62a28018484d
Ewerson santos <110944598+ewerson1@users.noreply.github.com>
2024-03-15 01:01:09 -0300

    Update script.js

--- Testes/002 - Loop/js/script.js
+++ Testes/002 - Loop/js/script.js
@@ -1,6 +1,21 @@
-let numbers = [1,2,3,4,5,2,5,2,1,2,3,4,5,2,5,2,];
+let numbers = [1, 2, 3, 4, 5, 2, 5, 2, 1, 2, 3, 4, 5, 2, 5, 2];
 
-
-for (let i = 0; i <= numbers.length; i++){
-  console.log(numbers[i])
+for (let i = 0; i <= numbers.length; i++) {
+  console.log(numbers[i]);
 }
+
+console.log("_________________________");
+
+let pessoa = ["batata", "colmeia", "sansão"];
+// push adiciona
+// pop remove no fim do array
+let pessoaRemovida = pessoa.pop();
+
+console.log(pessoa);
+console.log(pessoaRemovida);
+
+pessoa.push("batatinha")
+
+console.log(pessoa);
+
+console.log("_________________________");
