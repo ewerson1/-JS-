256e09a797c0a98ddc031204bae5ddaa6d134547
Ewerson santos <110944598+ewerson1@users.noreply.github.com>
2024-03-15 23:40:13 -0300

    json

---
+++ Testes/003-Json/js/script.js
@@ -0,0 +1,16 @@
+const motos = {
+  Escape: "Atala",
+  Motor: "2.0",
+  cor: "Vermelho",
+};
+
+let moto = JSON.stringify(motos);
+
+console.log(moto);
+
+let motoJSON = JSON.parse(moto);
+
+console.log(motoJSON);
+
+
+console.log(motoJSON.Motor);
